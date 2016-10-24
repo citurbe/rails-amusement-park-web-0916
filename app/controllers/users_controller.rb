15f2d4790a2827a8ab2ddaca6ceacf347a7effc7
Craig Iturbe <citurbe@gmail.com>
2016-10-24 18:07:38 -0400

    updated forms

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  skip_before_action :require_login, only: [:new, :create, :show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(session[:user_id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :happiness, :nausea, :tickets, :height)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  resources :sessions
+  resources :users
 
+  get '/', to: 'home#index'
+  get '/signin', to: 'users#new'
 
 end
